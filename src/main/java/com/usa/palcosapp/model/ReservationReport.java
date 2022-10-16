3d63550c2dc261860c1f4cb6921842c09c7061fa
olgapedrazamt <olga.pedraza.mt@usa.edu.co>
2022-10-16 13:04:30 -0500

    second commit

---
+++ src/main/java/com/usa/palcosapp/model/ReservationReport.java
@@ -0,0 +1,18 @@
+package com.usa.palcosapp.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReservationReport {
+    private Integer completed;
+    private Integer canceled;
+}
