5f51c08ce6f02dfea4c5a30ea682ee8ff3959c96
olgapedrazamt <olga.pedraza.mt@usa.edu.co>
2022-10-16 11:15:48 -0500

    first commit

---
+++ src/main/java/com/usa/palcosapp/model/Category.java
@@ -0,0 +1,29 @@
+package com.usa.palcosapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Description;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "category")
+    @JsonIgnoreProperties("category")
+    private List<Box> boxes;
+}
